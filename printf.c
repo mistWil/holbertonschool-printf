23e2be896c79af6c72dbd50fa32cc8bef91269c4
chloe0524 <chloco31@gmail.com>
2023-11-22 06:24:42 -0800

    added remove special case cuz betty not happy

--- printf.c
+++ printf.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-/**
- * _printf - prints to output according to format
- * @format: character string containing format specification
- *
- * Return: number of characters printed or -1 if format NULL
- */
 
-#include "main.h"
+/**
+ *_printf - prints to output according to format
+ *@format: character string containing format specification
+ *
+ *Return: number of characters printed or -1 if format NULL
+ */
 
 int _printf(const char *format, ...)
 {
@@ -17,36 +16,37 @@ int _printf(const char *format, ...)
 	int (*pDisplay)(va_list va);
 
 	va_start(list, format);
-	if (format == NULL)
-		return (-1);
-
 	while (format[i])
 	{
 		if (format[i] != '%')
 		{
-			_putchar(format[i]);
+			_putchar (format[i]);
 			ctr++;
+			i++;
+			continue;
 		}
+		i++;
+		pDisplay = isCharFormat(format[i]);
+		if (pDisplay != NULL)
+			ctr += pDisplay(list);
 		else
 		{
-			i++;
 			if (format[i] == '\0')
-				break;
-
-			pDisplay = isCharFormat(format[i]);
-			if (pDisplay != NULL)
-				ctr += pDisplay(list);
+			{
+				ctr = -1;
+				continue;
+			}
+			if (format[i] == '%')
+				_putchar('%');
 			else
 			{
-				_putchar('%');
+				_putchar(format[i - 1]);
 				_putchar(format[i]);
-				ctr += 2;
 			}
+			ctr += 2;
 		}
 		i++;
 	}
-
 	va_end(list);
 	return (ctr);
 }
-
