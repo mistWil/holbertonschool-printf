eb5ab95d8e7c6461e495701dd0d80f041778efb8
chloe0524 <chloco31@gmail.com>
2023-11-22 04:50:23 -0800

    change names of functions

--- printf.c
+++ printf.c
@@ -1,51 +1,49 @@
-#include <stdarg.h>
 #include "main.h"
+
 
 /**
  *_printf - prints to output according to format
- *@format: character string
+ *@format: character string containing format specification
  *
- *Return: number of characters printed
+ *Return: number of characters printed or -1 if format NULL
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0;
-	int (*fdisplay)(va_list);
-	va_list args;
+	va_list list;
+	int i = 0;
+	int ctr = 0;
+	int (*pDisplay)(va_list va);
 
-	va_start(args, format);
-	if (format == NULL || !format[i + 1])
+	va_start(list, format);
+	if (format == NULL)
 		return (-1);
+
 	while (format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1])
-			{
-				if (format[i + 1] != 'c' && format[i + 1] != 's'
-						&& format[i + 1] != '%' && format[i + 1] != 'd'
-						&& format[i + 1] != 'i')
-				{
-					j += _putchar(format[i]);
-					j += _putchar(format[i + 1]);
-					i++;
-				}
-				else
-				{
-					fdisplay = is_known_spec(&format[i + 1]);
-					j += fdisplay(args);
-					i++;
-				}
-			}
+			_putchar (format[i]);
+			ctr++;
+			i++;
+			continue;
+		}
+		/* C'est un % */
+		i++;
+		pDisplay = isCharFormat(format[i]);
+		if (pDisplay != NULL)
+		{
+			/* This is a known format character */
+			ctr += pDisplay(list);
 		}
 		else
 		{
 			_putchar(format[i]);
-			j++;
+			ctr++;
 		}
 		i++;
 	}
-	va_end(args);
-	return (j);
+
+	va_end(list);
+	return (ctr);
 }
