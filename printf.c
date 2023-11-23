d66a58a98f90707fa87e3d411d1eaa23b3a23f94
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2023-11-23 20:36:41 +0100

    Update printf.c change to snake case and simplified code

--- printf.c
+++ printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  *_printf - prints to output according to format
  *@format: character string containing format specification
@@ -13,7 +12,7 @@ int _printf(const char *format, ...)
 	va_list list;
 	int i = 0;
 	int ctr = 0; /* counter */
-	int (*pDisplay)(va_list list);
+	int (*p_display)(va_list list);
 
 	va_start(list, format);
 	if (format == NULL)
@@ -22,26 +21,24 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			_putchar (format[i++]);
+			_putchar (format[i]);
+			i++;
 			ctr++;
 			continue;
 		}
 		i++;
-		pDisplay = isCharFormat(format[i]); /* get format specifier function */
-		if (pDisplay != NULL)
-			ctr += pDisplay(list); /* call function and update the counter */
+		p_display = is_char_format(format[i]); /* get format specifier function */
+		if (p_display != NULL)
+			ctr += p_display(list); /* call function and update the counter */
 		else
 		{
 			if (format[i] == '\0')
-			{
-				ctr = -1;
-				continue;
-			}
+				return (-1);
 			if (format[i] == '%')
 				ctr += _putchar('%');
 			else
 			{
-				_putchar(format[i - 1]);
+				_putchar('%');
 				_putchar(format[i]);
 				ctr += 2;
 			}
